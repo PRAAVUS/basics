b36998b38169d73c0ab398e9ff562074eb8d3fa2
PRAAVUS <73282565+PRAAVUS@users.noreply.github.com>
2020-11-06 23:14:04 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+
+            int num1 = 1;
+            int num2 = 2;
+            int num3, num4, num5, num6;
+            num3 = 3;
+            num4 = 4;
+            num5 = 5;
+            num6 = 6;
+            int numsum = num3 + num4 + num5 + num6;
+           // Console.WriteLine(numsum);
+
+
+
+            
+           
+        }
+    }
+}
